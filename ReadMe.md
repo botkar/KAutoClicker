3ad7a6a730d48838db59b72131aad2b484e9b57c
Karim Besser <102550117+Karim-Besser@users.noreply.github.com>
2023-02-17 14:38:05 +0000

    first commit

---
+++ ReadMe.md
@@ -0,0 +1,11 @@
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+<h1 style="color: blue; font-weight: bolder; font-family: 'Poppins', sans-serif;"> Welcome to KAutoClicker</h1>
+KAutoClicker is a free, open source, cross-platform auto clicker for Windows and Mac OS. It is written in Python using the pyautogui. It is designed to be as simple to use as possible, with a simple Terminal interface.
+
+> ## Getting Started
+> ### Installation
+To install KAutoClicker, simply download it by git cloning the repo or downloading the zip file. Then, run the main.py file. after that you need to install the requirements by running the command `pip install -r requirements.txt` in the terminal.
+>### How to use
+To start the program, run the main.py file. You will be prompted to click enter to start the program after you click enter and wait for 5 seconds the program will start clicking. To stop the program, press ctrl+c in the terminal then it will stop clicking.
--- main.py
+++ main.py
@@ -1,42 +1,29 @@
 import pyautogui
 import time
 import os
-
-
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
-
-
 clear()
-app_name = "AutoClicker"
-
 
 class color:
-    PURPLE = '\033[95m'
-    CYAN = '\033[96m'
-    DARKCYAN = '\033[36m'
-    BLUE = '\033[94m'
-    GREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    RED = '\033[91m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-    END = '\033[0m'
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
 
-
-KeyboardInterrupt_Text = color.BOLD+color.RED + \
-    color.UNDERLINE+"Stopped Clicking!"+color.END
-error_text = color.BOLD+color.RED+color.UNDERLINE+"An error occured"+color.END
-welcome_input = color.CYAN+color.BOLD + \
-    "Press Enter to start the AutoClicker"+color.END
-welcome_text = color.BOLD+color.GREEN + \
-    color.UNDERLINE+"Welcome to "+app_name+"!"+color.END
-Started_Text = color.BOLD+color.GREEN+"Started clicking..."+color.END
-close_text_windows = color.BOLD+color.RED + \
-    color.UNDERLINE+"Press Ctrl+C to stop"+color.END
-close_text_macOS = color.BOLD+color.RED + \
-    color.UNDERLINE+"Press Cmd+C to stop"+color.END
-
+KeyboardInterrupt_Text=color.BOLD+color.RED+color.UNDERLINE+"Stopped Clicking!"+color.END
+error_text=color.BOLD+color.RED+color.UNDERLINE+"An error occured"+color.END
+welcome_input=color.CYAN+color.BOLD+"Press Enter to start the AutoClicker"+color.END
+welcome_text=color.BOLD+color.GREEN+color.UNDERLINE+"Welcome to KAutoClicker!"+color.END
+Started_Text=color.BOLD+color.GREEN+"Started clicking..."+color.END
+close_text_windows=color.BOLD+color.RED+color.UNDERLINE+"Press Ctrl+C to stop"+color.END
+close_text_macOS=color.BOLD+color.RED+color.UNDERLINE+"Press Cmd+C to stop"+color.END
 
 def main():
     try:
